7f9e718b0b18c83fa794932249b282ce944499a8
Michaela Meissner <m.p.meissner85@gmail.com>
2023-04-02 10:20:48 -0400

    Created models for PetProfiles App

--- backend/petprofiles/models.py
+++ backend/petprofiles/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from authentication.models import User
+from prescriptions import Prescription
 
 # Create your models here.
+class PetProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pet_name = models.CharField(max_length=255)
+    species = models.CharField(max_length=255)
+    breed = models.CharField(max_length=255)
+    date_of_birth = models.DateField(max_length=255)
+    prescriptions = models.ForeignKey(Prescription, on_delete=models.CASCADE)
+
+
+
