6ac6b293f54b92f99be1925ff6870153e6269425
Michaela Meissner <m.p.meissner85@gmail.com>
2023-04-09 16:03:16 -0400

    PrescriptionForm is functional but needs to be linked to a PetProfile

--- frontend/src/components/PrescriptionForm.jsx
+++ frontend/src/components/PrescriptionForm.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import useCustomForm from '../../hooks/useCustomForm';
-
-import useAuth from "../../hooks/useAuth";
-import axios from "axios";
+import useCustomForm from '../hooks/useCustomForm';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import useAuth from '../hooks/useAuth';
 
 
 let initialValues= {
@@ -13,13 +13,14 @@ let initialValues= {
 }
 
 const AddPrescription = () => {
+    const [petProfile, setPetProfile] = useState([])
     const [user, token]= useAuth()
     const navigate = useNavigate()
     const [formData, handleInputChange, handleSubmit] = useCustomForm(initialValues, createDrug)
 
     async function createDrug() {
         try {
-            let response = await axios.post('http://127.0.0.1:8000/drugs/make/', formData, {
+            let response = await axios.post('http://127.0.0.1:8000/both/all/', formData, {
                 headers: {
                     Authorization: 'Bearer ' + token
                 }
@@ -30,9 +31,19 @@ const AddPrescription = () => {
         }
     }
 
+//Note to self!! Name item doesn't work--FIX THIS!!
     return ( 
         <div className='container-md'>
-            <button>Add Medications</button>
+            <h3>Add Medications to {petProfile.pet_name} </h3> 
+            <form className='drug-form' onSubmit={handleSubmit}>
+                <label>Drug Name: {" "}
+                <input type='text' name='drug_name' value={formData.drug_name} onChange={handleInputChange}/></label><br></br>
+                <label>Drug Dose: {" "}
+                <input type='text' name='drug_dose' value={formData.drug_dose} onChange={handleInputChange}/></label><br></br>
+                <label>Drug Instructions: {" "}
+                <input type='text' name='drug_instr' value={formData.drug_instr} onChange={handleInputChange}/></label><br></br>         
+                <button>Add Medication</button>
+            </form>
         </div>
      );
 }
--- frontend/src/pages/AddPrescriptionPage/AddPrescriptionPage.jsx
+++ frontend/src/pages/AddPrescriptionPage/AddPrescriptionPage.jsx
@@ -1,40 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import useCustomForm from '../../hooks/useCustomForm';
-
-import useAuth from "../../hooks/useAuth";
-import axios from "axios";
-import { useNavigate } from 'react-router-dom';
+import PrescriptionForm from '../../components/PrescriptionForm';
 
 
-let initialValues= {
-    drug_name: " ",
-    drug_dose: " ",
-    drug_instr: " ",
-}
-
-const AddPrescription = () => {
-    const [user, token]= useAuth()
-    const navigate = useNavigate()
-    const [formData, handleInputChange, handleSubmit] = useCustomForm(initialValues, createDrug)
-
-    async function createDrug() {
-        try {
-            let response = await axios.post('http://127.0.0.1:8000/drugs/make/', formData, {
-                headers: {
-                    Authorization: 'Bearer ' + token
-                }
-            })
-            navigate('/profiles')
-        } catch (error) {
-            console.log(error.response.data)
-        }
-    }
+const AddPrescriptionPage = () => {
 
     return ( 
         <div className='container-md'>
-            <button>Add Medications</button>
+            <PrescriptionForm />
         </div>
      );
 }
  
-export default AddPrescription;
+export default AddPrescriptionPage;
