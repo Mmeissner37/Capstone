f655b3c634fd9ba23dad54f0455d9664ec80f9a4
Michaela Meissner <m.p.meissner85@gmail.com>
2023-04-09 12:54:36 -0400

    Started Component to Add Prescriptions to PetProfiles

---
+++ frontend/src/components/AddPrescription/AddPrescription.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import React, { useState, useEffect } from 'react';
+
+import useAuth from "../../hooks/useAuth";
+import axios from "axios";
+
+const AddPrescription = (props) => {
+    return ( 
+        <div>
+            <button>Add Medications</button>
+        </div>
+     );
+}
+ 
+export default AddPrescription;
