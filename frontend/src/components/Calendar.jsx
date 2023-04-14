d96f849dcbe43caec2db438c61aa3d6aca79413d
Michaela Meissner <m.p.meissner85@gmail.com>
2023-04-13 20:24:56 -0400

    Added Background Color to Calendar

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -8,30 +8,39 @@ import interactionPlugin from "@fullcalendar/interaction";
 export default class DemoApp extends React.Component {
     render() {
       return (
-        <FullCalendar
-          plugins={[ dayGridPlugin, interactionPlugin, timeGridPlugin ]}
-          initialView="dayGridMonth"
-          weekends={false}
-          dateClick={this.handleDateClick}
-          eventContent={this.render.eventContent}
-          headerToolbar={
-            {
-                start: 'today prev,next', // will normally be on the left. if RTL, will be on the right
-                center: 'title',
-                end: 'dayGridMonth,timeGridWeek,timeGridDay' // will normally be on the right. if RTL, will be on the left
-              }
-          }
-          businessHours={
-            {
-                start: '10:00',
-                end: '16:00',
-            }
-          }
-        />
+        <div className="calendar">
+            <FullCalendar
+                plugins={[ dayGridPlugin, interactionPlugin, timeGridPlugin ]}
+                initialView="dayGridMonth"
+                weekends={true}
+                dateClick={this.handleDateClick}
+                eventContent={this.render.eventContent}
+                viewHeight={690}
+                aspectRatio={2}
+                eventBackgroundColor="red"
+                headerToolbar={
+                    {
+                        start: 'today prev,next', // will normally be on the left. if RTL, will be on the right
+                        center: 'title',
+                        end: 'dayGridMonth,timeGridWeek,timeGridDay' // will normally be on the right. if RTL, will be on the left
+                    }
+                }
+                businessHours={
+                    {
+                        daysOfWeek: [1, 2, 3, 4],
+                        start: '10:00',
+                        end: '16:00',
+                    }
+                }
+            />
+        </div>
+        
       )
     }
 
   }
+
+
 
 
 
