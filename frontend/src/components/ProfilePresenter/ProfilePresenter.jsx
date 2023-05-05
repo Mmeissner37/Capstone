a91dfa0cb6c84339b38da33be74033e4b85601f1
Michaela Meissner <m.p.meissner85@gmail.com>
2023-05-05 18:11:37 -0400

    Added CSS to Pet Profile Presenter for Better User Experience

--- frontend/src/components/ProfilePresenter/ProfilePresenter.jsx
+++ frontend/src/components/ProfilePresenter/ProfilePresenter.jsx
@@ -40,15 +40,21 @@ const ProfilePresenter = () => {
                     <ol key={petprofile.id}>
                         <div className='present-profile'>
                             <h3>{petprofile.pet_name}</h3>
-                            <CreateImage petID={petprofile.id}/>
-                            <img src= {`http://127.0.0.1:8000${petprofile.image_url}/`} height='250px' width='200px' alt=''/>
-                            ID: {petprofile.id}<br></br>
-                            Name: {petprofile.pet_name}<br></br>
-                            Species: {petprofile.species}<br></br>
-                            Breed: {petprofile.breed}<br></br>
-                            Date of Birth: {petprofile.date_of_birth}<br></br>
-                            Medications: 
-                            <PetDrugs petID={petprofile.id} />
+                            <div className='indiv-profile'>
+                                ID: {petprofile.id}<br></br>
+                                {/* Name: {petprofile.pet_name}<br></br> */}
+                                Species: {petprofile.species}<br></br>
+                                Breed: {petprofile.breed}<br></br>
+                                Date of Birth: {petprofile.date_of_birth}<br></br>
+                                Medications: 
+                                <div>
+                                    <CreateImage petID={petprofile.id}/><br></br>
+                                    <img src= {`http://127.0.0.1:8000${petprofile.image_url}/`} height='250px' width='200px' alt='Profile image'/>
+                                </div>
+                            </div>
+                            <div className='profiledrugs'>
+                                <PetDrugs petID={petprofile.id} />
+                            </div>
                             <div className='drug-buttons'>
                                 <button onClick={()=> navigate('/createdrug')}>Add Medications To Profile</button>
                                 <button onClick={()=> navigate('/updatedrug')}>Update Medications</button>
